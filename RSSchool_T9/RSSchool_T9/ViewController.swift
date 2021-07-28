84a2aa1e9d2aa1ffd68a6727da30ad596ecac11d
Mikita <gentled.druzhinnik@gmail.com>
2021-07-28 12:00:20 +0300

    init: start rsschool_T9 project

---
+++ RSSchool_T9/RSSchool_T9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSSchool_T9
+//
+//  Created by Mikita Shalima on 28.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
